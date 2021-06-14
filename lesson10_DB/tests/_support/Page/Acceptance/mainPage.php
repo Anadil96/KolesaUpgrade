24e1f20ac57fbd366b18a8a91f0641fc54d5a2f1
Anadil96 <anadil.96.a@gmail.com>
2021-06-14 15:18:16 +0600

    Работа с mongodb

---
+++ lesson10_DB/tests/_support/Page/Acceptance/mainPage.php
@@ -0,0 +1,16 @@
+<?php
+namespace Page\Acceptance;
+
+class mainPage
+{
+    /**
+     * Урл для пользователя
+     */
+    public static $URL = '/?owner=AAnadil';
+
+    /**
+     * селктор кнопки snap
+     */
+    public static $snapBtn = '#snap';
+
+}
