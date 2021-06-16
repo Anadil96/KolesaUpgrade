df31ac590d99b280072140f9d4ef4acd8973a958
Anadil96 <anadil.96.a@gmail.com>
2021-06-16 23:41:42 +0600

    w

---
+++ lesson11_Pipeline/tests/acceptance/User1Cest.php
@@ -0,0 +1,41 @@
+<?php
+
+use Page\Acceptance\mainPage;
+use Step\Acceptance\AllStep;
+
+class User1Cest
+{
+    
+    /**
+     * для вызова функции создания пользователей
+     */
+    public function _before(\Step\Acceptance\AllStep $I)
+    {
+        $I->createUser();
+    }
+
+    /**
+     * Функция для проверки работы с сортировкой на удаление и сохранение
+     * @group 1
+     */
+    public function checkUsersModify(\Step\Acceptance\AllStep $I)
+    {
+        $I->amOnPage(mainPage::$URL);
+        $I->wait(5);
+
+        foreach($I->users as $val) {
+            $I -> waitForText($val ['name']);
+        }  
+
+        $I->saveAndDelete();
+        
+        $I -> click(mainPage::$snapBtn);
+        $I -> wait(5);
+
+        $I->checkKilledUsers();
+
+        $I->dontSeeInCollection('people', $I->killedBySnap);
+
+    }
+
+}
